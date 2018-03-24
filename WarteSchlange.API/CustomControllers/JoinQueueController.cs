50e59497b1993ee0e6c0ff9de589b4a27690e5b8
Robin Mader <r.mader@stud.uni-heidelberg.de>
2018-03-24 18:16:42 +0100

    Added JoinQueueController

---
+++ WarteSchlange.API/CustomControllers/JoinQueueController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WarteSchlange.API.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WarteSchlange.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/joinQueue")]
+    public class JoinQueueController : Controller
+    {
+
+        public QueueEntryErrorableModel JoinQueue(long queueId)
+        {
+            QueueEntryErrorableModel result = new QueueEntryErrorableModel();
+
+
+
+            return result;
+        }
+    }
+}
---
+++ WarteSchlange.API/ViewModels/QueueEntryErrorableModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WarteSchlange.API.Models;
+
+namespace WarteSchlange.API.ViewModels
+{
+    public class QueueEntryErrorableModel : QueueEntryModel
+    {
+        public bool HasError { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
