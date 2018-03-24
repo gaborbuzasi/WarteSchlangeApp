d7c84eb7b8d5be368e15bbc62ae03e85a7b8265c
Gabor Buzasi <gabor@sharein.com>
2018-03-24 13:28:59 +0000

    Added initial queuemodel and controller

---
+++ WarteSchlange.API/WarteSchlange.API/Models/QueueContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarteSchlange.API.Models
+{
+    public class QueueContext : DbContext
+    {
+        public QueueContext(DbContextOptions<QueueContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<QueueModel> Queues { get; set; }
+
+    }
+}
---
+++ WarteSchlange.API/WarteSchlange.API/Models/QueueModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarteSchlange.API.Models
+{
+    public class QueueModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }   
+}
