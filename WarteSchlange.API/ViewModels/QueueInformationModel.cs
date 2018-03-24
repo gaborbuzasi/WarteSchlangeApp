00cf02f86fb0d9104939db66f5a61f119569911d
Robin Mader <r.mader@stud.uni-heidelberg.de>
2018-03-24 21:59:38 +0100

    Implemented Information Request

--- WarteSchlange.API/ViewModels/QueueInformationModel.cs
+++ WarteSchlange.API/ViewModels/QueueInformationModel.cs
@@ -8,10 +8,10 @@ namespace WarteSchlange.API.ViewModels
     public class QueueInformationModel
     {
         
-        public float EstimatedWaitTime { get; set; }
+        public int EstimatedWaitTime { get; set; }
         public int QueuePosition { get; set; }
 
-        QueueInformationModel(float EstimatedWaitTime, int QueuePosition) {
+        public QueueInformationModel(int EstimatedWaitTime, int QueuePosition) {
             this.EstimatedWaitTime = EstimatedWaitTime;
             this.QueuePosition = QueuePosition;
         }
